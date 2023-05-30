1bbcd85d707f38737fe68d72304ac8c0ac9cddad
Alejandro Trejo <alextrejores@gmail.com>
2023-05-29 23:40:09 -0600

    Finished read methods

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
+using System.Data.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HPlusSport.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HPlusSport.Controllers
 {
@@ -18,9 +20,25 @@ namespace HPlusSport.Controllers
             _context.Database.EnsureCreated();
         }
 
+        // [HttpGet]
+        // public IEnumerable<Product> GetAllProducts(){
+        //     return _context.Products.ToArray();
+        // }
+
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts(){
-            return _context.Products.ToArray();
+        public async Task<ActionResult> GetAllProducts()
+        {
+            return Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = _context.Products.FindAsync(id);
+            if(product == null){
+                return NotFound();
+            }
+            return Ok( await product);
         }
     }
 }
