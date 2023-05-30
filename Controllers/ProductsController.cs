92dbb66c46db9a00c731bfdc370764537d999542
Alejandro Trejo <alextrejores@gmail.com>
2023-05-30 00:09:56 -0600

    Completed crud

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -40,5 +40,76 @@ namespace HPlusSport.Controllers
             }
             return Ok( await product);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product){
+            // if(!ModelState.IsValid){
+            //     return BadRequest();
+            // }
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id},
+                product
+            );
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Product>> UpdateProduct(int id, [FromBody] Product product){
+            if(id != product.Id){
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException) {
+                if(!_context.Products.Any(p => p.Id == id)){
+                    return NotFound();
+                }
+                else{
+                    throw;
+                }
+            }
+            
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id){
+            var product = await _context.Products.FindAsync(id);
+            if(product == null){
+                return NotFound();
+            }
+
+           _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<ActionResult<Product>> DeleteMultipleProducts([FromQuery]int[] ids){
+            var products = new List<Product>();
+            foreach(var id in ids)
+            {
+                var product = await _context.Products.FindAsync(id);
+
+                if(product == null){
+                    return NotFound();
+                }
+                products.Add(product);
+            }
+
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+
+            return Ok(products);
+        }
     }
 }
